b14bcb5194d7c0debddf8e951419c49fe119aa02
duongtai26495 <taiduong.264@gmail.com>
2022-03-29 16:28:56 +0700

    Create sourcebase

---
+++ src/main/java/net/accessory/hitechstore/histore/HiStoreApplication.java
@@ -0,0 +1,13 @@
+package net.accessory.hitechstore.histore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/accessory/hitechstore/histore/HiStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package net.accessory.hitechstore.histore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
